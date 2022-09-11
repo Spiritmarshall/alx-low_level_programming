2e402162eed79a2281b0cc20857e36f8eeb3772b
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-09-10 23:13:25 -0700

    101 correction description

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 /**
  * main - Entry point
  *
+ * Description: 'description for filedes *buf main and nbyte'
  * Return: Always 1 (Success)
  */
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61);
 		return (1);
 }
