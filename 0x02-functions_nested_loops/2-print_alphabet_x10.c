890678497a9c9a576aa6d19b38a513b1ba1058da
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-09-14 07:54:10 -0700

    Betty correction for task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10 - a function that prints 10 times the alphabet and a new line.
+ * print_alphabet_x10 - a function that prints 10 times the alphabet.
  *
- * Return: x10 a-z
- *
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	j = 0;
 
 	while (j < 10)
