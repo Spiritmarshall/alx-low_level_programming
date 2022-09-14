adbd5e8d8ee561cdc8711e3b695679f93df604da
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-09-14 07:24:18 -0700

    _putchar file correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
-/**main - Entry point
+/**
+ * main - Entry point
  *
- *Decsription: prints _putchar and a new line.
+ * Decsription: prints _putchar and a new line.
  *
  * Return:Always 0.
  */
-int main (void){
-	char c = "_putchar";
-	
-	while (c){
-		_putchar(c); c++;
+int main(void)
+{
+	char *c = "_putchar";
+
+	while (*c)
+	{
+		_putchar(*c);
+		c++;
 	}
 	_putchar('\n');
-
-return: 0;
+return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,15 @@
 #include<unistd.h>
 
 /**
- *_putchar - writes the character c to the standard output.
+ * _putchar - writes the character c to the standard output.
  *
  * @c: character
  *
- *return: on success 1. 
- On erreor,-1 is returned, and errn o is set.
+ * Return: On succes 1
+ * On fail -1 and errno is set.
  */
-int _putchar(char c){
-	return(write(1, &c, 1));
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
