bed0cfaa3e309c93c14e616695dbb55e62a61f35
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-09-14 02:17:13 -0700

    header file and _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include  "nested-loops.h"
+
+/**
+ * *_putchar - writes the character 'c' to the standard output
+ * 
+ * c: the Character
+ *
+ * Return: true
+ */
+int _putchar(char c){
+     write: _putchar; 
+	    return 0;
+}
