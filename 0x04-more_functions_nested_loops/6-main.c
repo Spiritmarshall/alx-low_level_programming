3f8b49d376af723db75c27e6683f1e4e80a2725f
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-09-17 01:30:09 -0700

    3 and 6 spacing fix

--- 0x04-more_functions_nested_loops/6-main.c
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-    print_line(0);
+	print_line(0);
 
-    print_line(2);
+	print_line(2);
 
-    print_line(10);
+	print_line(10);
 
-    print_line(-4);
+	print_line(-4);
 
-    return (0);
+	return (0);
 }
