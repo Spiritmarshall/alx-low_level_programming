85445401de7c98067555fa74ffbaf0cacc7b627c
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-09-21 20:48:02 -0700

    task 2 main.c

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -18,7 +18,7 @@ int main(void)
 		s1[i] = '*';
 	}
 	s1[i] = '\0';
-	rintf("%s\n", s1);
+	printf("%s\n", s1);
 	p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
 	printf("%s\n", s1);
 	printf("%s\n", p);
