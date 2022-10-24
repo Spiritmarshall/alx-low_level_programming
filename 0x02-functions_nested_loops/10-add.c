373a7ed73ccfacce0a342b92931f422879e276ec
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-10-23 18:00:06 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @ME: first input
+ * @YOU: second input
+ * Return: ME + YOU
+ */
+
+int add(int ME, int YOU)
+{
+	int MEYOU;
+
+	{
+		MEYOU = (ME + YOU);
+	}
+	return (MEYOU);
+}
