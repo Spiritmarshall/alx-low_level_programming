3ba9571b1e99c29b2f44cb43879dc5da8bd5289b
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-10-23 18:32:06 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: 'description for filedes *buf main and nbyte'
- * Return: Always 1 (Success)
- */
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main(void)
+#include <unistd.h>
 
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
