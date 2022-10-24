25d3b9a806af6c20529ea1ccc1253a666100494c
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-10-23 18:10:08 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always (successfuly)
+ */
+
+int main(void)
+{
+	int n = 1;
+	int CAL = 0;
+
+	while (n < 1024)
+	{
+		if (n % 3 == 0)
+			CAL += n;
+		else if (n % 5 == 0)
+			CAL += n;
+		n++;
+	}
+	printf("%d\n", CAL);
+	return (0);
+}
