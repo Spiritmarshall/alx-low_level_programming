a6cb4efe59401f00297a01b0b31dd0bcb4be5941
Onitiju Darasimi <prof.olutosin@gmail.com>
2022-11-01 14:05:07 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int is_prime(int n, int zz);
+
+/**
+ * is_prime_number - returns the 1 if n is prime
+ *	@n: number to be checked
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int zz = n / 2;
+
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, zz));
+}
+
+/**
+ * is_prime - returns the 1 if n is prime
+ *	@n: number to be checked
+ *	@zz: number to start checking from
+ * Return: 1 if n is prime, 0 otherwise
+ */
+
+int is_prime(int n, int zz)
+{
+	if (zz <= 1)
+		return (1);
+	else if (n % zz == 0)
+		return (0);
+	return (is_prime(n, zz - 1));
+}
